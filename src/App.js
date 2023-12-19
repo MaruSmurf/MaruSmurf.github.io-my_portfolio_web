8e03aa8f5d84187526cc7b6456d7d87a0646f787
Maria <marija.aks@seznam.cz>
2023-12-19 15:40:39 +0100

    add files to github

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import Main from "./Main"
+import HeroSection from "./components/HeroSection/HeroSection";
+import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer"
+
+function App() {
+  return (
+
+
+              <div className="App">
+                  <Navbar />
+                  <HeroSection/>
+                  <Skills/>
+                  <Projects/>
+                  <Footer/>
+
+
+              </div>
+
+
+  );
+}
+
+export default App;
